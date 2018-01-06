eceff749c9dffa3c39e1340a7845289de252b38f
Colin Casazza <Colanzio5@gmail.com>
2018-01-06 13:00:09 -0800

    echo server work

--- Rocket/DummyData.py
+++ Rocket/DummyData.py
@@ -4,11 +4,6 @@ import math
 import datetime
 import paho.mqtt.client as mqtt
 
-
-def packmessage():
-    ret = str("{ \"time\": \"" + gettime() + "\", \"d1\": " + getd1() + ", \"d2\": " + getd2() + ", \"d3\": " + getd3() + ", \"v1\": \"" + getv1() + "\", \"v2\": \"" + getv2() + "\" }").encode()
-    return ret
-
 def mqtt_init():
     print("attempting new mqtt init...")
     client = mqtt.Client()
@@ -26,6 +21,7 @@ def echo_data():
     random.seed(seed_data)
     
     #create fake valve states (randomly open small percentage of time)
+    #normally server would read data from the valve feedback sensor  
     # layout -> [v0, v1, v2...v7]
     relays = 8 * [1]
     for relay in relays:
@@ -33,22 +29,22 @@ def echo_data():
     print(relays)
 
     #create fake pressure transducer and thermocouple data
+    #normally server would read data from the ADC board
     # layout -> [p0, p1, p2, p3, t0, t1, t2, t3...t9]
     sensors = 14 * [1]
     increment = 0
     for sensor in sensors:
-        sensor = sin(increment + seed_data)
+        sensor = math.sin(increment + seed_data)
         increment += 0.25
     print(sensors)
 
-    alerts = 
+    # PUBLISH ANY OUTGOING DATA HERE
+    #publish generated values to mqtt broker - data will become available for clients
+    client.publish(valve_states_topic, ",".join(str(x) for x in relays))
+    client.publish(data_sensors_topic, ",".join(str(x) for x in sensors))
 
-        
-
+    return relays + sensors
     
-
-
-
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
     return rc
@@ -64,6 +60,13 @@ def on_message(client, userdata, msg):
 def parse_message(message):
     print(message)
 
+def main_loop():
+    client.loop_forever()
+
+    while True:
+        print("Echo Data: " + echo_data())
+        time.sleep(0.5)
+
 # OBJECTS #
 # Topic Name | Topic Description
 # 1 | Valve States  (publish)
@@ -73,18 +76,21 @@ def parse_message(message):
 # 5 | Safe/Alert/Panic/Launch (publish/subscribe)
 #       ...(0 - Safe, 1 - Alert, 2 - Panic, 3 - Abort, 4 - Launch)
 
+# Panic State
 SAFE_ALERT_PANIC_ABORT_LAUNCH = 0 
+
+# IP address of mqtt broker - localhost/127.0.0.1 if on local machine
 host = "192.168.1.132"	
+
+#mqtt broker topic string definitions
 valve_states_topic = "1"
 valve_command_topic = "2"
 data_sensors_topic = "3"
 data_telemetry_topic = "4"
 safe_abort_topic = "5"
+
+#init mqtt client connection and begin echo/listen loop
 client = mqtt_init()
+main_loop()
 
-# Begin Script
-while True:
-
-
-
-
+# end of program
